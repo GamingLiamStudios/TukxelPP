2af439bc0462a9748081747a7e59f9ab9223ed9a
GamingLiamStudios <swimmingliam@gmail.com>
2020-07-13 16:52:21 +1000

    Game Loop

--- src/Tukxel++.cpp
+++ src/Tukxel++.cpp
@@ -1,10 +1,13 @@
 ﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <chrono>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void update(GLFWwindow* window);
+void render(GLFWwindow* window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -36,16 +39,30 @@ int main() {
     }
     std::cout << "Initalized GLAD" << std::endl;
 
+    auto lastTime = std::chrono::high_resolution_clock::now();
+    double ammountOfTicks = 20.0;
+    double ns = 1000000000.0 / ammountOfTicks;
+    double delta = 0;
+    long timer = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch())
+        .count();
+    int frames = 0;
     while (!glfwWindowShouldClose(window)) {
-        //Input
-        processInput(window);
-
-        //Rendering
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        auto now = std::chrono::high_resolution_clock::now();
+        delta += std::chrono::duration_cast<std::chrono::nanoseconds>(now - lastTime).count() / ns;
+        lastTime = now;
+        while (delta >= 1) {
+            update(window);
+            delta--;
+        }
+        if (!glfwWindowShouldClose(window))
+            render(window);
+        frames++;
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch())
+                .count() - timer > 1000) {
+            timer += 1000;
+            std::cout << "FPS: " << frames << std::endl;
+            frames = 0;
+        }
     }
 
     glfwTerminate();
@@ -60,4 +77,19 @@ void processInput(GLFWwindow* window) {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void update(GLFWwindow* window) {
+    processInput(window);
+}
+
+void render(GLFWwindow* window) {
+    //Clear Screen
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    //Set Screen
+    glfwSwapBuffers(window);
+    glfwPollEvents();
 }
