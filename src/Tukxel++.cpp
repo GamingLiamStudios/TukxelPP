58fcc21b4713a637945a93aa4b3ce15a502a8eac
GamingLiamStudios <swimmingliam@gmail.com>
2020-07-13 16:26:33 +1000

    Fixed CMake again. Created White Window

--- src/Tukxel++.cpp
+++ src/Tukxel++.cpp
@@ -1,10 +1,63 @@
-﻿#include <iostream>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std;
+#include <iostream>
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    std::cout << "Initalized GLFW" << std::endl;
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    std::cout << "Created GLFW window" << std::endl;
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    std::cout << "Initalized GLAD" << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        //Input
+        processInput(window);
+
+        //Rendering
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    std::cout << "Terminated GLFW" << std::endl;
+    return 0;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
