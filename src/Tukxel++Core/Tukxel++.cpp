974a706c6d9d23f66bbf2a1a39de64a2d1308411
GamingLiamStudios <swimmingliam@gmail.com>
2020-07-16 00:28:45 +1000

    When you fail a commit

---
+++ src/Tukxel++Core/Tukxel++.cpp
@@ -0,0 +1,232 @@
+﻿#define WIREFRAME 0
+#define FPS_CAP_ENABLED 0
+#define FPS_CAP 240
+#define VSYNC 1
+#define TICK_COUNT 20
+#define FULLSCREEN 0
+
+#include "Tukxel++.h"
+
+#include <iostream>
+#include <chrono>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+float vertices[] = {
+    // positions          // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+     0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+};
+unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+};
+
+
+unsigned int VBO, VAO, EBO, texture;
+
+int main(void) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    std::cout << "Initalized GLFW" << std::endl;
+
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+#if FULLSCREEN
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Tukxel++", glfwGetPrimaryMonitor(), NULL);
+#else
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Tukxel++", NULL, NULL);
+    glfwSetWindowPos(window, mode->width / 2 - SCR_WIDTH / 2, mode->height / 2 - SCR_HEIGHT / 2);
+#endif
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    std::cout << "Created GLFW window" << std::endl;
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    std::cout << "Initalized GLAD" << std::endl;
+
+    glfwSwapInterval(VSYNC);
+#if WIREFRAME
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+#endif
+    stbi_set_flip_vertically_on_load(true);
+
+    Shader shader("res/shaders/VertexShader.txt", "res/shaders/FragmentShader.txt");
+    if (init(shader) != 0) {
+        dispose();
+        glfwTerminate();
+        std::cin.get();
+        return -1;
+    }
+    std::cout << "Initalized" << std::endl;
+    long long lastTime = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    double ns = 1000000000.0 / TICK_COUNT;
+#if FPS_CAP_ENABLED
+    double fns = 1000000000.0 / FPS_CAP;
+    double fdelta = 0;
+#elif VSYNC
+    double fns = 1000000000.0 / mode->refreshRate;
+    double fdelta = 0;
+#endif
+    double delta = 0;
+    long long timer = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    int frames = 0;
+    while (!glfwWindowShouldClose(window)) {
+        long long now = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+        delta += (now - lastTime) / ns;
+        while (delta >= 1) {
+            update(window);
+            delta--;
+        }
+#if FPS_CAP_ENABLED || VSYNC
+        fdelta += (now - lastTime) / fns;
+        if (!glfwWindowShouldClose(window) && fdelta >= 1) {
+            render(window, shader);
+            fdelta--;
+            frames++;
+        }
+#else
+        if (!glfwWindowShouldClose(window))
+            render(window, shader);
+        frames++;
+#endif
+        lastTime = now;
+        if (std::chrono::high_resolution_clock::now().time_since_epoch().count() - timer > 1000000000) {
+            timer += 1000000000;
+            std::cout << "FPS: " << frames << std::endl;
+            frames = 0;
+        }
+    }
+
+    dispose();
+    glfwTerminate();
+    std::cout << "Terminated GLFW" << std::endl;
+    return 0;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    glfwSwapInterval(VSYNC);
+}
+
+int bindTexture(unsigned int& ID, const char* location) {
+    //Init Texture
+    glGenTextures(1, &ID);
+    glBindTexture(GL_TEXTURE_2D, ID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //Load Texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(location, &width, &height,
+        &nrChannels, 0);
+    if (!(width && (!(width & (width - 1)))) || !(height && (!(height & (height - 1))))) {
+        std::cout << "Texture at \"" << location << "\" is not power of 2" << std::endl;
+        return -1;
+    }
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+            GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+        return -1;
+    }
+    stbi_image_free(data);
+    return 0;
+}
+
+int init(Shader shader) {
+    int error = 0;
+    //Load Triangles
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //Bind Textures
+    error |= bindTexture(texture, "res/pog.jpg");
+    shader.use();
+    shader.setInt("texture1", 0);
+
+    return error;
+}
+
+void dispose() {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+void update(GLFWwindow* window) {
+    //Update shit
+}
+
+void render(GLFWwindow* window, Shader shader) {
+    //Process Input
+    processInput(window);
+
+    //Clear Screen
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    //Enable Shader
+    shader.use();
+
+    //Apply Transform
+    glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    // retrieve the matrix uniform locations
+    unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
+    unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
+    // pass them to the shaders (3 different ways)
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+    shader.setMat4("projection", projection);
+
+    //Draw
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    //Set Screen
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
